e79aff0264acc825c3a502533d4089fee04bb436
İsa Kulaksız <isakulaksiz@outlook.com>
2022-07-15 12:30:32 +0300

    init textfield area

---
+++ app/src/main/java/com/example/noteapp/components/NoteComponent.kt
@@ -0,0 +1,29 @@
+package com.example.noteapp.components
+
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun NoteInputText(modifier: Modifier = Modifier,
+    text: String,
+    label: String,
+    maxLine: Int = 1,
+    onTextChange: (String) -> Unit,
+    onImeAction: () -> Unit = {}){
+    val keyboardController = LocalSoftwareKeyboardController.current
+    BasicTextField(value = text,
+        onValueChange = onTextChange,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyboardController!!.hide()
+        }), modifier = modifier
+    )
+}
--- app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
@@ -7,11 +8,13 @@ import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.noteapp.R
+import com.example.noteapp.components.NoteInputText
 
 @Composable
 fun NoteScreen(){
@@ -22,6 +25,14 @@ fun NoteScreen(){
             Icon(imageVector = Icons.Rounded.Notifications,
             contentDescription = "Icon")
         })
+
+        // Content area
+        Column(modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            NoteInputText(text = "hello",
+                label = "Helo",
+                onTextChange = {})
+        }
     }
 }
 
