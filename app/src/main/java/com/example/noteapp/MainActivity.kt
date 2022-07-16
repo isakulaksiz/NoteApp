641cd348b6d785ddb96fc876237171950db97c61
İsa Kulaksız <isakulaksiz@outlook.com>
2022-07-16 11:42:57 +0300

    implements adding and removing notes

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.noteapp
 
 import NoteScreen
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.noteapp.data.NotesDataSource
+import com.example.noteapp.model.Note
 import com.example.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,7 +31,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
+                    val notes = remember{
+                        mutableStateListOf<Note>()
+                    }
+                    NoteScreen(notes = notes,
+                        onAddNote = {
+                                      notes.add(it)
+                    }, onRemoveNote = {
+                                notes.remove(it)
+                    })
                 }
             }
         }
--- app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
@@ -1,4 +1,5 @@
 import android.os.Build
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,6 +41,7 @@ fun NoteScreen(
     var description by remember{
         mutableStateOf("")
     }
+    val context = LocalContext.current
     Column(modifier = Modifier.padding(6.dp)) {
         SmallTopAppBar(title = {
                                 Text(text = stringResource(id = R.string.app_name))
@@ -74,17 +77,19 @@ fun NoteScreen(
 
             NoteButton(text = "SAVE", onClick = {
                 if(title.isNotEmpty() && description.isNotEmpty()){
-                    // listeye ekle
+                    onAddNote(Note(title = title, description = description))
                     title = ""
                     description = ""
-
+                    Toast.makeText(context, "Added Note", Toast.LENGTH_SHORT).show()
                 }
             })
         }
         //Divider(color = Color.MAGENTA, modifier = Modifier.fillMaxWidth().width(2.dp))
         LazyColumn{
             items(notes){ note->
-                NoteRow(note = note, onNoteClicked = {})
+                NoteRow(note = note, onNoteClicked = {
+                    onRemoveNote(note)
+                })
 
             }
         }
@@ -105,7 +110,7 @@ fun NoteRow(modifier: Modifier = Modifier,
         shadowElevation = 6.dp){
         Column(
             modifier
-                .clickable { }
+                .clickable {onNoteClicked(note) }
                 .padding(
                     horizontal = 14.dp,
                     vertical = 6.dp
