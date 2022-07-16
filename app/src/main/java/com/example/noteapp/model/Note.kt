78ddee9e6eaaa25fd61700c9fd7dc029931f96b7
İsa Kulaksız <isakulaksiz@outlook.com>
2022-07-16 16:50:56 +0300

    added note entity

--- app/src/main/java/com/example/noteapp/model/Note.kt
+++ app/src/main/java/com/example/noteapp/model/Note.kt
@@ -1,11 +1,26 @@
 package com.example.noteapp.model
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
 import java.util.*
 
-data class Note(
+@Entity(tableName = "notes_tbl")
+data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey
     val id:UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
+
+    @ColumnInfo(name = "note_entry_date")
     val entryDate: LocalDateTime = LocalDateTime.now()
+    //val entryDate: Date = Date.from(Instant.now())
 )
